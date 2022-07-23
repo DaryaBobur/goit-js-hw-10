cbb4bde11d7ff725fd2d4cdec2337cfa0587bfd8
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-07-23 19:39:06 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,12 @@
-export default function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`).then(response => response.json())
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .catch(error => {
+ console.log(error);
+  });
 }
